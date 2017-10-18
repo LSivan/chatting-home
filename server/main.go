1b76708f5a434e8b28a8ad80c3eda32f9c8e1dde
L_Sivan <907616591@qq.com>
2017-10-18 14:35:01 +0800

    封装withMapTraversal函数

--- server/main.go
+++ server/main.go
@@ -8,11 +8,11 @@ import (
 	"strconv"
 )
 
-var conn_map = make(map[string]net.Conn)
-var nickname_map = make(map[string]string)
+var connMaps = make(map[string]net.Conn)
+var nicknameMaps = make(map[string]string)
 
 func main() {
-
+	log.Printf("The server is listening on 127.0.0.1:1798\n")
 	listener, err := net.Listen("tcp", "localhost:1798")
 	if err != nil {
 		log.Fatalf("net.Listen err----------->%v\n", err)
@@ -39,44 +39,51 @@ func headleConn(conn net.Conn) {
 	for scanner.Scan() {
 		// 第一次输入，保存昵称
 		if isNickname {
-			nickname_map[addr] = scanner.Text()
+			nickname := scanner.Text()
+			if nickname == "" {
+				nickname = "user from "+addr
+			}
+			nicknameMaps[addr] = nickname
 			// 保存会话
-			_, ok := conn_map[addr]
+			_, ok := connMaps[addr]
 			if !ok {
-				conn_map[addr] = conn
+				connMaps[addr] = conn
 			}
-			conn.Write([]byte("Hello " + scanner.Text() +  ",chat with others and enjoy it.\n"))
+			conn.Write([]byte("Hello " + nickname  +  ",chat with others and enjoy it.\n"))
+			conn.Write([]byte(strconv.Itoa(len(connMaps))+" person at the chatting home this time!\n"))
 			// 遍历map发送欢迎消息
-			for _addr, connect := range conn_map {
-				if _addr == addr {
-					// 不发自己
-					continue
-				}
-				mustWrite(connect,[]byte( "Welcome " + scanner.Text() + " to join in the chatting home! "),_addr)
-				mustWrite(connect,[]byte( strconv.Itoa(len(conn_map))+" person at the chatting home this time!\n"),_addr)
-			}
+			withMapTraversal(addr,func(_addr string,connect net.Conn){
+				mustWrite(connect,[]byte( "\nWelcome " + scanner.Text() + " to join in the chatting home! "),_addr)
+				mustWrite(connect,[]byte( strconv.Itoa(len(connMaps))+" person at the chatting home this time!\n\n"),_addr)
+			})
 			isNickname = false
 		} else {
 			// 检测到输入,先在发话的窗口输出
-			conn.Write([]byte(nickname_map[addr] + ":" + scanner.Text() + "\n"))
+			conn.Write([]byte(nicknameMaps[addr] + "(you said):" + scanner.Text() + "\n"))
 			// 遍历map发送消息
-			for _addr, connect := range conn_map {
-				if _addr == addr {
-					// 不发自己
-					continue
-				}
-				mustWrite(connect,[]byte(nickname_map[addr] + ":" + scanner.Text() + "\n"),_addr)
-			}
+			withMapTraversal(addr,func(_addr string,connect net.Conn){
+				mustWrite(connect,[]byte(nicknameMaps[addr] + ":" + scanner.Text() + "\n"),_addr)
+			})
 		}
 
 	}
 	conn.Close()
 }
 
+func withMapTraversal(addr string,fn func(string,net.Conn)){
+	for _addr, connect := range connMaps {
+		if _addr == addr {
+			// 不发自己
+			continue
+		}
+		fn(_addr,connect)
+	}
+}
+
 func mustWrite(conn net.Conn,word []byte,addr string){
 	i, err := conn.Write(word)
 	if err != nil {
 		fmt.Printf("err-----------%v,i----------->%d", err, i)
-		delete(conn_map, addr)
+		delete(connMaps, addr)
 	}
 }
